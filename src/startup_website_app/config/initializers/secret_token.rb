1c0dcd3bad4c18cc27839c3d1bc4268c9586310e
Kathy LeVine <Kathy@KatherineELeVine-6.local>
2013-07-01 18:48:19 -0400

    initialized rails framework

---
+++ src/startup_website_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StartupWebsiteApp::Application.config.secret_token = 'efbff37a624b53208ba649735068fbe2d8e2e503a7221fbd50cdae74ab2dd683cb9ee9e547334dfd23a2bfa8fdca2620a9132b13bf1b96bb239471ccaeaba313'
